eb1a6a067d127aa946bc8722942f2a950793d3b8
MD MOHSIN NUR ROBIN <mrobin132@icloud.com>
2023-01-11 00:32:36 +0100

    Update 15count_repeated_characters.py

--- 2. strings (level 2)/15count_repeated_characters.py
+++ 2. strings (level 2)/15count_repeated_characters.py
@@ -1,3 +1,19 @@
 # count repeated characters
 
- 
+s = "Hello"
+
+count = 0
+repeated_char = []
+
+for char in s:
+    if (s.count(char) > 1) and (char not in repeated_char):
+        count = count + 1
+        repeated_char.append(char)
+
+print(count)
+
+if (len(repeated_char) > 0):
+    for char in sorted(repeated_char):
+        print(char, end = " ")
+else:
+    print("None")    
