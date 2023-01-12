b9552eee896076879e60a1a79cd6184de1cd83be
MD MOHSIN NUR ROBIN <mrobin132@icloud.com>
2023-01-12 17:49:45 +0100

    update

---
+++ 5. dictionaries (level 1)/37.py
@@ -0,0 +1,10 @@
+my_dict = {"a": 14, "b": 3, "c": 7}
+
+get_value = list(my_dict.values())
+
+if len(get_value) == 0:
+    print("None")
+else:
+    for values in get_value:
+        print(max(get_value))
+        break
